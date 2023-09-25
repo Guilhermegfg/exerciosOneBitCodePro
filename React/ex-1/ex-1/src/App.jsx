d9021a893ca02e953202c2f6b0f00b6e92be11dd
Guilherme Fernades <guilhermegfg2011@hotmail.com>
2023-09-25 11:25:46 -0300

    update

---
+++ React/ex-1/ex-1/src/App.jsx
@@ -0,0 +1,33 @@
+export default function App () {
+    return (
+        <div className="app">
+            <img src="react.png" alt="" />
+            <h1>React</h1>
+            <p>A biblioteca para interfaces de usuário web e nativas</p>
+            <button>Aprenda React</button>
+            <button>Referência da API</button>
+            <hr />
+            <h2>Crie interfaces de usuário de componetes</h2>
+            <p>React permite que você construa interfaces de usuário a partir de pedaços
+                individuais chamados componentes
+            </p>
+            <hr />
+            <h2>Escreva componenetes com código e marcação</h2>
+            <p>Componentes React são funções JavaScript. A sintaxe de 
+                marcação é chamada JSX. É uma extensão da sintaxe Javascript 
+                popularizada pelo react.
+            </p>
+            <hr />
+            <h2>Próximos passos</h2>
+            <ul>
+                <li>Uso de dados dinâmicos no JSX</li>
+                <li>Criação de novos componentes</li>
+                <li>Estilização de componentes</li>
+                <li>Reutilização de componentes</li>
+                <li>Uso de props e children</li>
+                <li>Uso de eventos do javascript</li>
+            </ul>
+
+        </div>
+    )
+}
